851da9495623226dcd2386bd091c1a267533dc88
novamind <annodonaris@gmail.com>
2024-07-23 09:52:04 +0200

    WIP question answering

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,16 +8,29 @@ def get_summarizer():
 
 
 summarizer = get_summarizer()
-st.title("🦜🔗 Content Summarizer")
+st.title("🦜🔗 Content Assistant")
 with st.form("my_form"):
     url = st.text_area(
         "Enter a link:",
-        "https://example.com",
+        "https://example.com"
     )
-    submitted = st.form_submit_button("Submit")
+    submitted = st.form_submit_button("Summarize")
     if submitted:
         with st.spinner("Summarizing..."):
             docs = summarizer.load_docs(url)
             result = summarizer.summarize_docs(docs)
             st.success("Done!")
             st.markdown(result)
+
+with st.form("my_form2"):
+    # TODO: load docs only once   
+    docs = summarizer.load_docs(url)
+    question = st.text_area(
+        "Ask a question:",
+        "What is this website about?"
+    )
+    asked = st.form_submit_button("Ask")
+    if asked:
+        with st.spinner("Answering..."):
+            response = summarizer.get_answer(docs, question)
+            st.markdown(response["answer"])
--- summarizer.py
+++ summarizer.py
@@ -3,6 +3,12 @@ import requests
 from langchain_community.llms import Ollama
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.chains.summarize import load_summarize_chain
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain_core.prompts import ChatPromptTemplate
 
 
 class OllamaSummarizer:
@@ -34,6 +40,40 @@ class OllamaSummarizer:
         result = summary["output_text"]
         return result
 
+    def get_answer(self, docs, question):
+
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500)
+        documents = text_splitter.split_documents(docs)
+
+        OLLAMA_HOST = os.environ.get("OLLAMA_HOST")
+        embeddings = OllamaEmbeddings(base_url=f"http://{OLLAMA_HOST}")
+
+        vector = Chroma.from_documents(documents, embeddings)
+        system_prompt = (
+            """
+                Answer the user's questions based on the below context. 
+                If the context doesn't contain any relevant information to the question, don't make something up and just say "I don't know":
+
+                <context>
+                {context}
+                </context>
+                """
+        )
+
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", system_prompt),
+                ("human", "{input}"),
+            ]
+        )
+
+        document_chain = create_stuff_documents_chain(self.llm, prompt)
+        retriever = vector.as_retriever()
+        retrieval_chain = create_retrieval_chain(retriever, document_chain)
+        response = retrieval_chain.invoke({"input": question})
+
+        return response
+
 
 if __name__ == "__main__":
 
@@ -44,5 +84,6 @@ if __name__ == "__main__":
     ]
     docs = summarizer.load_docs(links)
 
-    result = summarizer.summarize_docs(docs)
-    print(result)
+    # result = summarizer.summarize_docs(docs)
+    # print(result)
+    summarizer.get_answer(docs)
